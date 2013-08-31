2404583c764d2dcba8f8e36f06036d98c0dabe3d
Marconi Moreto <caketoad@gmail.com>
2013-08-31 23:38:53 +0800

    auto-mount each method handler

--- main.go
+++ main.go
@@ -7,11 +7,17 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"reflect"
+	"strings"
 
 	"github.com/gorilla/mux"
 	"github.com/marconi/jsonstash/bucket"
 	"github.com/marconi/jsonstash/utils"
 )
+
+type BucketPayload struct {
+	Key string
+}
 
 type RESTView struct {
 	Router *mux.Router
@@ -24,18 +30,52 @@ func NewRestView() *mux.Router {
 		Stash:  bucket.NewStash(),
 	}
 
-	rv.Router.HandleFunc("/buckets", rv.GetListBuckets).Methods("GET")
-	rv.Router.HandleFunc("/buckets", rv.PostListBuckets).Methods("POST")
+	// mount views
+	bucketView := &BucketView{RESTView: rv, Stash: rv.Stash}
+	rv.AddView("/buckets", bucketView, []string{"GET", "POST"})
 
 	return rv.Router
 }
 
-func (rv *RESTView) GetListBuckets(w http.ResponseWriter, r *http.Request) {
-	names := rv.Stash.GetBucketNames()
-	rv.JSONResponse(w, r, names)
+func (rv *RESTView) AddView(path string, view interface{}, methods []string) {
+	// factory function that returns a handler wrapper
+	wrapper := func(method *reflect.Value) func(w http.ResponseWriter, r *http.Request) {
+		f := func(w http.ResponseWriter, r *http.Request) {
+			wVal := reflect.ValueOf(w)
+			rVal := reflect.ValueOf(r)
+			method.Call([]reflect.Value{wVal, rVal})
+		}
+		return f
+	}
+
+	// add handler for each method
+	for _, m := range methods {
+		m = strings.Title(strings.ToLower(m))
+		method := reflect.ValueOf(view).MethodByName(m)
+		rv.Router.HandleFunc(path, wrapper(&method)).Methods(m)
+	}
 }
 
-func (rv *RESTView) PostListBuckets(w http.ResponseWriter, r *http.Request) {
+func (rv *RESTView) JSONResponse(w http.ResponseWriter, r *http.Request, data []string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(utils.ToJSON(data)))
+}
+
+func (rv *RESTView) Response(w http.ResponseWriter, r *http.Request, data string) {
+	w.Write([]byte(data))
+}
+
+type BucketView struct {
+	RESTView *RESTView
+	Stash    *bucket.Stash
+}
+
+func (bv *BucketView) Get(w http.ResponseWriter, r *http.Request) {
+	names := bv.Stash.GetBucketNames()
+	bv.RESTView.JSONResponse(w, r, names)
+}
+
+func (bv *BucketView) Post(w http.ResponseWriter, r *http.Request) {
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Println("Error reading body: ", err)
@@ -59,23 +99,9 @@ func (rv *RESTView) PostListBuckets(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// we have a valid payload
-	rv.Stash.Add(payload.Key)
+	bv.Stash.Add(payload.Key)
 	msg := fmt.Sprintf("Bucket %s has been added.", payload.Key)
-	rv.Response(w, r, msg)
-	return
-}
-
-func (rv *RESTView) JSONResponse(w http.ResponseWriter, r *http.Request, data []string) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(utils.ToJSON(data)))
-}
-
-func (rv *RESTView) Response(w http.ResponseWriter, r *http.Request, data string) {
-	w.Write([]byte(data))
-}
-
-type BucketPayload struct {
-	Key string
+	bv.RESTView.Response(w, r, msg)
 }
 
 func main() {
